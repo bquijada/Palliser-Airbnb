7e7d238ff152aa95a8ad851e23408cb2e7f542f1
bquijada <bqlevy@gmail.com>
2023-09-09 16:00:34 -0600

    color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,15 @@
-import { HStack, Icon, Image, Text } from '@chakra-ui/react'
-import React from 'react'
-import {GiMountains} from 'react-icons/gi'
+import { HStack, Icon, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import { GiMountains } from "react-icons/gi";
+import ColorSwitch from "./ColorSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
-        <Icon as={GiMountains} w={20} h={20}/>
-        <Text>NavBar</Text>
+    <HStack justifyContent='space-between' padding='10px'>
+      <Icon as={GiMountains} w={20} h={20} />
+      <ColorSwitch></ColorSwitch>
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
