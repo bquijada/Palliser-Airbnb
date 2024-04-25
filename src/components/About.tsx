007e8085cbdfdd2f07e45056fcdbd2e9285abbdd
bquijada <bqlevy@gmail.com>
2024-04-25 15:48:02 -0700

    text edits

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,9 +14,7 @@ const About = () => {
         <NavBar></NavBar>
       </GridItem>
       <GridItem area="main">
-        <h1 className="heading">
-          About Us: Brenda & Clément
-        </h1>
+        <h1 className="heading">About Us: Brenda & Clément</h1>
         <HStack>
           <Image
             boxSize="500px"
@@ -25,22 +23,26 @@ const About = () => {
             border="5px solid gray"
             rounded="md"
           ></Image>
-          <Text marginLeft={2} w="600px"
-          fontWeight="medium" fontSize="xl">
-            Clem and I initially came to Golden by accident 3 years ago and
-            absolutely fell in love with the town and the ski resort. We loved
-            it so much we decided to make it our permanent home. Clem is from
+          <Text
+            textAlign="left"
+            marginLeft={3}
+            w="600px"
+            fontWeight="medium"
+            fontSize="xl"
+          >
+            Clem and I first discovered Golden by accident 3 years ago and
+            absolutely fell in love with the town and Kicking Horse. We loved it
+            here so much we decided to make it our permanent home. Clem is from
             France and I'm from California and we met on a ski season in Niseko,
-            Japan in 2019. Situated on the ground floor of the Palliser, our
-            cozy Airbnb awaits, offering direct access to the slopes right from
-            your porch. Our B unit is a separate lock-off from our main condo,
-            ensuring your privacy and comfort. Since acquiring this little piece
-            of mountain paradise in May 2023, we've been thrilled to welcome
-            guests from all corners of the globe and share the magic of Golden
-            with them. Living up on the hill has been a dream come true for us,
-            and we're excited to extend that same experience to you. Come and
-            experience the beauty of Golden firsthand – we can't wait to host
-            you!
+            Japan in 2019. Since acquiring this little piece of mountain
+            paradise in May 2023, we've been thrilled to welcome guests from all
+            corners of the globe and share the magic of Golden with them.
+            Situated on the ground floor of the Palliser, you can ski from the
+            slopes right to your porch! Our B unit is a separate lock-off from
+            our main condo, ensuring your privacy and comfort. Living up on the
+            hill has been a dream come true for us, and we're excited to extend
+            that same experience to you. Come and experience the beauty of
+            Golden firsthand – we can't wait to host you!
           </Text>
         </HStack>
       </GridItem>
