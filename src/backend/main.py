cbb6d96116415a7595e0d6708e28bb55f615bfb8
bquijada <bqlevy@gmail.com>
2024-05-13 13:35:57 -0700

    edit cors link

--- src/backend/main.py
+++ src/backend/main.py
@@ -8,9 +8,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.secret_key = "123456789"
 client = datastore.Client(project="palliserbnb")
-cors = CORS(app, resources={r"/activity/winter": {"origins": "https://palliserairbnb.vercel.app/"},
-                            r"/activity/summer": {"origins": "https://palliserairbnb.vercel.app/"},
-                            r"/tag": {"origins": "https://palliserairbnb.vercel.app/"}})
+cors = CORS(app, resources={r"/activity/winter": {"origins": "https://palliserairbnb.vercel.app"},
+                            r"/activity/summer": {"origins": "https://palliserairbnb.vercel.app"},
+                            r"/tag": {"origins": "https://palliserairbnb.vercel.app"}})
 
 app.register_blueprint(activity.bp)
 app.register_blueprint(tag.bp)
